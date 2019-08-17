ce458704a9cbb2e3e6a66c93342cf1dd9b21abff
NobuyaOsawa <nobuya.osawa@gmail.com>
2019-08-18 01:04:06 +0900

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,52 @@
+class ArticlesController < ApplicationController
+  def index
+    # Articleモデルを経由して、DBのarticlesテーブルの中身を全て@articlesに代入？ってこと？
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    # ▼テスト用_formから渡ってきた値をviewとして表示する
+    # render plain: @article.inspect
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+end
